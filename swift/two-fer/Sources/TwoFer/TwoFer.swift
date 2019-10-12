cc228e8ce284c5683a78e5320ded467f8364c37a
Bettina Weinholtz <bettina.weinholtz@tum.de>
2019-10-12 23:53:04 +0200

    Add TwoFer solution in Swift

---
+++ swift/two-fer/Sources/TwoFer/TwoFer.swift
@@ -0,0 +1,9 @@
+final class TwoFer {
+    static func twoFer() -> String {
+        return "One for you, one for me."
+    }
+    
+    static func twoFer(name:String) -> String {
+        return "One for \(name), one for me."
+    }
+}
