26526907647fb5922b063303c0f52e34e12d2ec4
Bettina Weinholtz <bettina.weinholtz@tum.de>
2019-10-13 00:23:23 +0200

    Add Leap solution in Swift

---
+++ swift/leap/Sources/Leap/Leap.swift
@@ -0,0 +1,22 @@
+//Solution goes in Sources
+import Foundation
+
+class Year {
+    let year: Int
+    
+    var isLeapYear: Bool {
+        if (year % 400 == 0) {
+            return true
+        } else if (year % 100 == 0) {
+            return false
+        } else if (year % 4 == 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    init(calendarYear: Int) {
+        self.year = calendarYear
+    }
+}
