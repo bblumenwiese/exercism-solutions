b0fe7489aeb278b3597371387460b0d42aa72414
Bettina Weinholtz <bettina.weinholtz@tum.de>
2019-10-20 19:06:14 +0200

    Leap Year in Swift: Turn computed property into static property

--- swift/leap/Sources/Leap/Leap.swift
+++ swift/leap/Sources/Leap/Leap.swift
@@ -1,19 +1,20 @@
 class Year {
-    let year: Int
+    public let year: Int
+    public let isLeapYear: Bool
     
-    var isLeapYear: Bool {
-        if (year % 400 == 0) {
-            return true
-        } else if (year % 100 == 0) {
-            return false
-        } else if (year % 4 == 0) {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    init(calendarYear: Int) {
+    public init(calendarYear: Int) {
         self.year = calendarYear
+        self.isLeapYear = checkIsLeapYear(year: calendarYear)
     }
+}
+
+fileprivate func checkIsLeapYear(year: Int) -> Bool {
+    if (year % 400 == 0) {
+        return true
+    } else if (year % 100 == 0) {
+        return false
+    } else if (year % 4 == 0) {
+        return true
+    }
+    return false
 }
